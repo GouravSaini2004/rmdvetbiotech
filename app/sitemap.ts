07f82582b7fe5f825f2bc433887826d0a995b6a8
GouravSaini <113535932+GouravSaini2004@users.noreply.github.com>
2026-09-05 09:04:37 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://rmdvetbiotech.com",
+      url: "https://www.rmdvetbiotech.com/",
       lastModified: new Date(),
     },
     {
-      url: "https://rmdvetbiotech.com/aboutUs",
+      url: "https://www.rmdvetbiotech.com/aboutus",
       lastModified: new Date(),
     },
     {
-      url: "https://rmdvetbiotech.com/contact",
+      url: "https://www.rmdvetbiotech.com/contact",
       lastModified: new Date(),
     },
     {
-      url: "https://rmdvetbiotech.com/products",
+      url: "https://www.rmdvetbiotech.com/products",
       lastModified: new Date(),
     },
   ];
