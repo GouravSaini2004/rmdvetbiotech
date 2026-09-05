27a84863d17b6c4ecb37d2780d8cb5e5c8d9405f
GouravSaini <113535932+GouravSaini2004@users.noreply.github.com>
2026-09-05 09:32:13 +0530

    Update page.tsx

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -1,15 +1,26 @@
-import MissionVision from "../../components/missionvission"
-import AboutHero from "../../components/about"
-import CoreValues from "../../components/corevalues"
-import ContactEnquiry from "../../components/contact"
+import type { Metadata } from "next";
+
+import MissionVision from "../../components/missionvission";
+import AboutHero from "../../components/about";
+import CoreValues from "../../components/corevalues";
+import ContactEnquiry from "../../components/contact";
+
+export const metadata: Metadata = {
+  title: "About Us | RMD Vet Biotech",
+  description:
+    "Learn about RMD Vet Biotech, our mission, vision, core values, and commitment to animal health and nutrition.",
+  alternates: {
+    canonical: "/aboutUs",
+  },
+};
 
 export default function About() {
-    return(
-        <>
-        <AboutHero />
-        <MissionVision />
-        <CoreValues />
-        {/* <ContactEnquiry /> */}
-        </>
-    )
+  return (
+    <>
+      <AboutHero />
+      <MissionVision />
+      <CoreValues />
+      {/* <ContactEnquiry /> */}
+    </>
+  );
 }
